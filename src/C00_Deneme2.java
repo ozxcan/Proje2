64199019bc9346ba6e01e95c6aaebd70260fd244
özcan <ozxcan91@gmail.com>
2023-07-18 14:54:16 +0200

    3.Degisim

--- src/C00_Deneme2.java
+++ src/C00_Deneme2.java
@@ -2,5 +2,6 @@ public class C00_Deneme2 {
     public static void main(String[] args) {
         System.out.println("C00_Deneme2 ");
         System.out.println("C00_Deneme3 ");
+        System.out.println("C00_Deneme4 ");
     }
 }
